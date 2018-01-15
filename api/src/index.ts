313b4260c0ad12c43ae58a02c6552fdfd705c3cf
Joseph Luck <joseph.luck@sky.com>
2018-01-15 10:24:37 +0000

    Add typeorm

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,34 +1,38 @@
-import config from '../../config'
-
+import 'reflect-metadata'
+import { createConnection } from 'typeorm'
 import * as Hapi from 'hapi'
-import validate from './utils/authentication'
+import config from '../../config'
+import authentication from './utils/authentication'
 import routes from './routes'
+import entities from './entities'
 
-const db = {
-  get: {
-    users: () => ({
-      john: {
-        username: 'john',
-        password:
-          '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm', // 'secret'
-        name: 'John Doe',
-        id: '2133d32a',
-      },
-    }),
-  },
+async function createDatabase() {
+  return createConnection({
+    type: 'postgres',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'admin',
+    database: 'test',
+    entities: entities(),
+    synchronize: true,
+    logging: true,
+  })
 }
 
-const main = async () => {
+const startServer = async (db: any) => {
+  console.log(db)
   const server = Hapi.server({ port: config.local.apiPort })
 
-  await validate(server, db)
+  await authentication(server, db)
   routes(server, db)
 
   await server.start()
   return server
 }
 
-main()
+createDatabase()
+  .then(startServer)
   .then(server =>
     console.log(`Server listening on ${server.info.uri}`),
   )
